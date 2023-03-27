bf84a2a6c17bcddb546627435531e6cf9a2ccfcd
A-Cobble <alex.cobble42@gmail.com>
2023-03-27 14:24:28 -0700

    Practiced pass by reference. Learned the difference between pass by reference and pass by value

---
+++ passByReference/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+//The & symbol represents pass by reference. 
+//This means that it refers to the actual value/ actual location of the parameter and doesn't create a copy of the parameter
+void pass_by_ref1(int &num);
+void pass_by_ref2(string &s);
+void pass_by_ref3(vector <string> &v);
+void print_vector(const vector <string> &v);
+
+void pass_by_ref1(int &num) {
+	num = 1000;
+}
+
+void pass_by_ref2(string &s) {
+	s = "Changed";
+}
+
+void pass_by_ref3(vector <string> &v) {
+	v.clear(); // delete all vector elements
+}
+
+void print_vector(vector <string> &v) {
+	for (auto s : v) {
+		cout << s << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+
+	int num{ 10 };
+	int another_num{ 20 };
+
+	cout << "num before calling pass_by_ref1: " << num << endl;
+
+	//passes in a copy of num and thus does not change num
+	pass_by_ref1(num);
+	cout << "num after calling pass_by_ref1: " << num << endl;
+
+	cout << "\nanother_num before calling pass_by_ref1: " << another_num << endl;
+	pass_by_ref1(another_num);
+	cout << "\nanother_num after calling pass_by_ref1: " << another_num << endl;
+
+	string name{ "Alexander" };
+	cout << "\nname before calling pass_by_ref2: " << name << endl;
+	pass_by_ref2(name);
+	cout << "\nname after calling pass_by_ref2: " << name << endl;
+
+	vector <string> stooges{ "Larry", "Moe", "Curly" };
+	cout << "\nstooges before calling pass_by_ref3: ";
+	print_vector(stooges);
+	pass_by_ref3(stooges);
+	cout << "stooges after calling pass_by_ref3: ";
+	print_vector(stooges);
+
+	cout << endl;
+	return 0;
+}
