252a4eabfa1ca7a8379d9d81f90282a33a9f0410
A-Cobble <alex.cobble42@gmail.com>
2023-03-28 17:21:36 -0700

    Commented out a section to for readability

--- recursiveFunctions/main.cpp
+++ recursiveFunctions/main.cpp
@@ -7,12 +7,12 @@ using std::endl;
 unsigned long long fibonacci(unsigned long long n);
 unsigned long long factorial(unsigned long long n);
 
-unsigned long long fibonacci(unsigned long long n) {
-	if (n <= 1) {
-		return n; // base case
-	}
-	return fibonacci(n - 1) + fibonacci(n - 2); //recursion
-}
+//unsigned long long fibonacci(unsigned long long n) {
+//	if (n <= 1) {
+//		return n; // base case
+//	}
+//	return fibonacci(n - 1) + fibonacci(n - 2); //recursion
+//}
 
 unsigned long long factorial(unsigned long long n) {
 	if (n == 0) {
